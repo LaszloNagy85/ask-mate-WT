c262c8be0251e79ce78c7d35b1a5f50698a688e9
Armin Kovacs <armin0191@gmail.com>
2019-08-19 18:24:28 +0200

    Ask question implemented with export function

--- data_manager.py
+++ data_manager.py
@@ -3,3 +3,7 @@ import connection
 
 def get_all_data(filename):
     return connection.get_all_data_from_file(filename)
+
+
+def export_data(filename, input_data, data_header):
+    return connection.export_data_to_file(filename, input_data, data_header)
