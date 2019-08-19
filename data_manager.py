7fcec896b71d539381fc0b94abbe3411adec9ebe
LaszloNagy85 <n.laszlo.uk@gmail.com>
2019-08-19 19:57:09 +0200

    Add sort function with default values.

--- data_manager.py
+++ data_manager.py
@@ -3,3 +3,13 @@ import connection
 
 def get_all_data(filename):
     return connection.get_all_data_from_file(filename)
+
+
+def get_sorted_data(filename, sort_by='submission_time', order_direction='desc'):
+    data_to_sort = get_all_data(filename)
+
+    is_int = str if sort_by == 'title' else int
+    is_reverse = True if order_direction == 'desc' else False
+
+    sorted_questions = sorted(data_to_sort, key=lambda x: is_int(x[sort_by]), reverse= is_reverse)
+    return sorted_questions
