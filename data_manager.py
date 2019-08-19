929d7a4d6cddf0dc69eb635bb85275174b763b04
LaszloNagy85 <n.laszlo.uk@gmail.com>
2019-08-19 21:41:09 +0200

    Remove default values from sort function.

--- data_manager.py
+++ data_manager.py
@@ -5,13 +5,13 @@ def get_all_data(filename):
     return connection.get_all_data_from_file(filename)
 
 
-def get_sorted_data(filename, sort_by='submission_time', order_direction='desc'):
+def get_sorted_data(filename, sort_by, order_direction):
     data_to_sort = get_all_data(filename)
 
     is_int = str if sort_by == 'title' else int
     is_reverse = True if order_direction == 'desc' else False
 
-    sorted_questions = sorted(data_to_sort, key=lambda x: is_int(x[sort_by]), reverse= is_reverse)
+    sorted_questions = sorted(data_to_sort, key=lambda x: is_int(x[sort_by]), reverse=is_reverse)
     return sorted_questions
 
 
