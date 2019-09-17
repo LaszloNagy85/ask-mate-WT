2d2efdaa4156cb260e8ccce88cfed45e92f7434b
Armin Kovacs <armin0191@gmail.com>
2019-09-17 11:32:12 +0200

    Fix typo

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, session, escape
+from flask import Flask, request, render_template, redirect, url_for, session
 import data_manager
 import util
 
